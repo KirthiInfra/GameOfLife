5690fd609146fdebee15cceb79dad49b9bc63007
KirthiInfra <kirthivasan@infraspec.dev>
2025-07-26 19:06:45 +0530

    Create the board structure and add test

--- gameOfLIfe.go
+++ gameOfLIfe.go
@@ -1,1 +1,16 @@
 package gameoflife
+
+type Board struct {
+    board [][]int
+}
+
+func NewBoard(m, n int) *Board {
+    board := make([][]int, m)
+    for i := range board {
+        board[i] = make([]int, n)
+        for j := range board[i] {
+            board[i][j] = 0
+        }
+    }
+    return &Board{board: board}
+}
--- gameOfLIfe_test.go
+++ gameOfLIfe_test.go
@@ -1,1 +1,11 @@
 package gameoflife
+
+import "testing"
+
+func TestNewBoard(t *testing.T) {
+    board := NewBoard(3, 3)
+    
+    if len(board.board) != 3 || len(board.board[0]) != 3 {
+        t.Fatalf("expected 3x3 board, got %dx%d", len(board.board), len(board.board[0]))
+    }
+}
